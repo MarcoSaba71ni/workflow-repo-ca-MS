74008ca71f46459576895648ef6ab1c27a6325bf
Marco Sabatini <marsab04188@stud.noroff.no>
2025-10-01 21:37:19 +0200

    Test two

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,13 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-
+ 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: {
-      globals: globals.browser,
-      require: true,
-      module: true,
-      process: true,
-      describe: true,
-      test: true,
-      it: true,
-      expect: true,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-    },
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
   },
 ]);
+ 
