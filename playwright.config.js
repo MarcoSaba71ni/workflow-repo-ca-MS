89b133c1bfb61913a5e862c49b390d0cd9920846
Marco Sabatini <marsab04188@stud.noroff.no>
2025-10-13 17:08:38 +0200

    many changes

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,15 @@
 // @ts-check
 
 import { defineConfig, devices } from '@playwright/test';
-
+import dotenv from 'dotenv';
+import path from 'path';
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
 // import dotenv from 'dotenv';
 // import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-require('dotenv').config();
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
