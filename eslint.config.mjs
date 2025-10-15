a00f67c784c3bffee3f34782b3f0a222916ced70
Marco Sabatini <marsab04188@stud.noroff.no>
2025-10-15 09:54:50 +0200

    eslint config changes on global.browser

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,16 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser }, //add global vars
+    languageOptions: { 
+      globals:  { ...globals.browser, 
+      describe: true, // Used for grouping tests
+        test: true, // Used to create tests
+        it: true, // Alternative way to create tests
+        expect: true, // Used for test assertions
+        require: true, // Used in Node.js files like Tailwind config
+        module: true, // Used in Node.js files like Tailwind config
+        process: true, }
+    }, 
   },
 ]);
  
